b2145df91c6e382634b43b9101571188ff7cb8b0
reinhardt <s_reinhardt@outlook.de>
2023-06-12 15:05:16 +0200

    Update 08_baboon_island.py

--- 03_Logical_Operators/challenges/08_baboon_island.py
+++ 03_Logical_Operators/challenges/08_baboon_island.py
@@ -11,47 +11,4 @@
 
 # Dein Coder nach dieser Zeile:
 
-inventar = ""
 print("~~~~Willkommen auf Baboon Island~~~~\n")
-print("Irgendwo auf dieser Insel ist eine goldene Pavian-Statue. Finde Sie und überlebe.\n")
-decision = input("Du hast am Strand angelegt. Du kannst entweder weiter am \"Strand\" laufen oder in den \"Dschungel\""
-                 "\ngehen. Was tust du?\n").lower()
-if decision == "dschungel":
-    decision = input("Du kämpfst dich durch den dichten Dschungel, kommst aber kaum voran. Mit der Machete könntest "
-                     "\ndu dir einen Weg frei schlagen. Nutzt du deine \"Machete\" oder gehst du so \"weiter\"?\n")\
-                .lower()
-    if decision == "machete":
-        print("\nSchon beim ersten Schwung mit der Machete wird dir klar, dass du mit der Machete nicht umgehen kannst."
-              "\nDu rutscht ab, schneidest ein dünnes Stück Holz ab, welches als fieser kleine Splitter nun in deiner "
-              "\nHand steckt. Jede Bewegung mit der Hand tut nun voll weh.\n~~~Dein Abenteuer endet hier.~~~")
-    else:  # weiter
-        decision = input("Du kommst an einer Steintafel an auf der folgendes geschrieben steht: \"Womit jagen Paviane "
-                         "Fische?\"\nDarunter sind zwei herausgehobene Bilder zu sehen. Ein \"Speer\" und \"Käscher\""
-                         ".\nWorauf drückst du?\n").lower()
-        if decision == "speer":
-            inventar = "banane"
-        else:  # käscher
-            inventar = "auge"
-        print("Nach dem du den halben Tag gesucht hast, kommst du an eine Stelle an der sich zwei Palmen überkreuzen. "
-              "\nEine davon hat eine bananenförmige Vertiefung.")
-        if inventar == "banane":
-            print("Du legst die goldene Banane in die Vertiefung. Erst passiert eine Weile nichts, doch als du gerade "
-                  "\ngehen willst, sagt die linke Palme: \"Danke!\". Zwischen den Palmen knarrt es. Der Boden beginnt "
-                  "\netwas zu wackeln und es öffnet sich eine verborgene Bodenklappe genau zwischen den beiden Bäumen."
-                  "\nDu schaust hinein und findest: Die goldene Pavianstatue."
-                  "\n~~~Glückwunsch. Du hast das Spiel gemeistert.~~~")
-        else:  # auge
-            print("Du läufst zurück in den Dschungel und holst dutzende Bananen von verschiedenen Bäumen. Doch keine "
-                  "\nwill so recht passen. Eine Gruppe Paviane, die gerade vom Fischen kommt, läuft auf dich zu. Sie "
-                  "\nhalten dir einen empörten Vortrag über Lebensmittelverschwendung und Schädigung des Ökosystems. "
-                  "\n~~~Dein Abenteuer endet hier.~~~")
-else:
-    decision = input("Du triffst auf eine Gruppe Paviane, die am Speerfischen sind. Du kannst dich \"vorstellen\" "
-                     "\noder \"fliehen\". Was tust du?\n").lower()
-    if decision == "fliehen":
-        print("Du stolperst über eine zerbrochene Riesenmuschel und schneidest dir dabei den Fuß auf."
-              "\n~~~Dein Abenteuer endet hier.~~~")
-    else:  # vorstellen
-        print("Du gehst zu den Pavianen, stellst dich vor und schüttelst ein paar Hände. Nach einer halben Stunde "
-              "\nbist du sicher, dass du das Jobinterview gemeistert hast. Doch die Paviane haben nie zurückgerufen. "
-              "\n~~~Dein Abenteuer endet hier.~~~")
