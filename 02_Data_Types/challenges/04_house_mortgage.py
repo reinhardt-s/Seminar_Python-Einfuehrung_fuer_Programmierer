5bd5e4858d393bfa6a05b82361c9e8ead4bd307a
reinhardt <s_reinhardt@outlook.de>
2023-07-31 11:21:01 +0200

    Update 04_house_mortgage.py

--- 02_Data_Types/challenges/04_house_mortgage.py
+++ 02_Data_Types/challenges/04_house_mortgage.py
@@ -1,2 +1,11 @@
 # Schreiben Sie ein Programm, welche die restliche Kreditlaufzeit berechnet und in Jahren, Monaten, wochen und Tagen
 # ausgibt
+
+years_total = 25
+payed_years = input("Wie viele Jahre hast du schon abbezahlt: ")
+# Schreiben Sie Ihren Code unter dieser Zeile
+
+# Dein Code über dieser Zeile
+
+print(f"Du muss noch {remaining_years} Jahre oder {remaining_months} Monate oder {remaining_weeks} Wochen oder {remaining_days} Tage den Kredit "
+      f"abbezahlen")
